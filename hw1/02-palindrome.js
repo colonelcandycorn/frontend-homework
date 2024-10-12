3463b6e4bf10e8ba41ba6b188ce1a23171081562
Sarah Dylan <SarahmDylan@gmail.com>
2024-10-12 14:45:38 -0700

    Remove unnecessary enum

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,56 +1,48 @@
 const elem = document.querySelector("input");
 const palindromeText = document.getElementById("palindrome");
 
-const Status = Object.freeze({
-  VALID_PALINDROME: 0,
-  INVALID_PALINDROME: 1,
-  NEGATIVE_NUM: 2,
-  NON_NUMERIC: 3,
-  EMPTY: 4,
-});
-
 const handleInput = () => {
   const value = elem.value;
   palindromeText.innerText = "";
   palindromeText.classList.remove("text-success", "text-danger");
 
-  switch (evaluateNumber(value)) {
-    case Status.VALID_PALINDROME:
-      palindromeText.innerText = "Yes this is a palindrome";
-      palindromeText.classList.add("text-success");
-      break;
-    case Status.INVALID_PALINDROME:
-      palindromeText.innerText = "No. Try again.";
-      palindromeText.classList.add("text-danger");
-      break;
-    case Status.NEGATIVE_NUM:
-      palindromeText.innerText = "Negative number";
-      palindromeText.classList.add("text-danger");
-      break;
-    case Status.NON_NUMERIC:
-      palindromeText.innerText = "Non-numeric characters detected.";
-      palindromeText.classList.add("text-danger");
-      break;
-    case Status.EMPTY:
-      palindromeText.innerText = "";
-      break;
+  if (value.length === 0) {
+    palindromeText.innerText = "";
+    return;
+  }
+
+  if (Number.isNaN(value) || value.includes(" ")) {
+    palindromeText.innerText = "Non-numeric characters detected.";
+    palindromeText.classList.add("text-danger");
+    return;
+  }
+
+  if (parseFloat(value) < 0) {
+    palindromeText.innerText = "Negative number";
+    palindromeText.classList.add("text-danger");
+    return;
+  }
+
+  if (isPalindrome(value)) {
+    palindromeText.innerText = "Yes. this is a palindrome!";
+    palindromeText.classList.add("text-success");
+  } else {
+    palindromeText.innerText = "No. Try again.";
+    palindromeText.classList.add("text-danger");
   }
 };
-const evaluateNumber = (num) => {
-  if (num.length === 0) return Status.EMPTY;
-  if (Number.isNaN(num)) return Status.NON_NUMERIC;
-  if (parseFloat(num) < 0) return Status.NEGATIVE_NUM;
 
+const isPalindrome = (num) => {
   let l = 0;
   let r = num.length - 1;
 
   while (l <= r) {
     if (num[l++] !== num[r--]) {
-      return Status.INVALID_PALINDROME;
+      return false;
     }
   }
 
-  return Status.VALID_PALINDROME;
+  return true;
 };
 
 elem.addEventListener("input", handleInput);
