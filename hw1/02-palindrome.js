aa47f86b21cf270cc29f6a07ac244c3b7aa6d359
Sarah Dylan <95723213+colonelcandycorn@users.noreply.github.com>
2024-10-19 14:10:52 -0700

    Finish HW1 (#1)
    * Finish the first activity and update gitignore
    
    * Finish HW1
    
    * Remove unnecessary enum
    
    * Fix linting errors
    
    * Fix error with loop
    
    * Update HTML based on feedback

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,50 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+const palindromeText = document.getElementById("palindrome");
 
-elem.addEventListener('input', handleInput);
+const isPalindrome = (num) => {
+  let l = 0;
+  let r = num.length - 1;
+
+  while (l <= r) {
+    if (num[l] !== num[r]) {
+      return false;
+    }
+    l += 1;
+    r -= 1;
+  }
+
+  return true;
+};
+
+const handleInput = () => {
+  const { value } = elem;
+  palindromeText.innerText = "";
+  palindromeText.classList.remove("text-success", "text-danger");
+
+  if (value.length === 0) {
+    palindromeText.innerText = "";
+    return;
+  }
+
+  if (Number.isNaN(value) || value.includes(" ")) {
+    palindromeText.innerText = "Non-numeric characters detected.";
+    palindromeText.classList.add("text-danger");
+    return;
+  }
+
+  if (parseFloat(value) < 0) {
+    palindromeText.innerText = "Negative number";
+    palindromeText.classList.add("text-danger");
+    return;
+  }
+
+  if (isPalindrome(value)) {
+    palindromeText.innerText = "Yes. this is a palindrome!";
+    palindromeText.classList.add("text-success");
+  } else {
+    palindromeText.innerText = "No. Try again.";
+    palindromeText.classList.add("text-danger");
+  }
+};
+
+elem.addEventListener("input", handleInput);
