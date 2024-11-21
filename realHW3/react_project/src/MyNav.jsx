91646fe2ea5b3c7e4579ab42badc6a7f7db6200d
Sarah Dylan <SarahmDylan@gmail.com>
2024-11-20 18:51:29 -0800

    Adjust based on feedback

--- realHW3/react_project/src/MyNav.jsx
+++ realHW3/react_project/src/MyNav.jsx
@@ -6,23 +6,23 @@ import { LinkContainer } from 'react-router-bootstrap';
 const MyNav = () => {
   return (
     <>
-      <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar bg="dark" data-bs-theme="dark">
         <Container className="m-0">
           <Navbar.Brand as={Link} to="/">
             Homework 3
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Nav className="justify-content-end me-auto">
-            <Nav.Link as={Link} to="/home">
+            <Nav.Link as={Link} to="/home" className="text-light">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/list">
+            <Nav.Link as={Link} to="/list"className="text-light">
               List
             </Nav.Link>
-            <Nav.Link as={Link} to="/population">
+            <Nav.Link as={Link} to="/population" className="text-light">
               Population
             </Nav.Link>
-            <Nav.Link as={Link} to="/gdp">
+            <Nav.Link as={Link} to="/gdp" className="text-light">
               GDP
             </Nav.Link>
           </Nav>
